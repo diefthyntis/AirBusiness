337fa82b67ad2e3fbe92b18e2011f3b77527e73a
Alan Hauser <diefthyntis@gmail.com>
2024-08-20 10:47:20 +0200

    Application initiale tel que générée par Spring Initializr

---
+++ src/main/java/com/diefthyntis/AirBusiness/AirBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.diefthyntis.AirBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diefthyntis/AirBusiness/AirBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diefthyntis.AirBusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
